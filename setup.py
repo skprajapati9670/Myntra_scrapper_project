1273b5c1246ca48d7fb82b3da1ed00bf0fcbd014
unknown <skprajapati9670@gmail.com>
2024-09-28 19:57:00 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='scrapper',
+    version='0.0.1',
+    author='Sumit',
+    author_email='skprajapati9670@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
